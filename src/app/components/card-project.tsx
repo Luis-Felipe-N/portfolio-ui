cb347f794f59a1d4e3bbcf944b98b681d79f43fc
luis.nunes <luisj2felipe09@gmail.com>
2023-09-09 21:31:26 -0300

    :lipstick: Card with opticional backdrop

--- src/app/components/card-project.tsx
+++ src/app/components/card-project.tsx
@@ -1,27 +1,38 @@
-import Image from "next/image";
-import { Button } from "./ui/button";
-import { Fraunces } from "next/font/google";
+import Image from 'next/image'
+import { Fraunces } from 'next/font/google'
+
+import { Project } from '@/page'
+import Link from 'next/link'
+
+interface CardProjectProps {
+  project: Project
+}
 
 const fraunces = Fraunces({ subsets: ['latin'] })
 
-export function CardProject() {
+export function CardProject({ project }: CardProjectProps) {
   return (
-    <div className='w-full py-12'>
+    <Link href={project.preview}>
+      <div className="w-96 py-12 px-8 bg-slate-900 rounded-lg">
+        <div className=" flex justify-center h-64">
           <Image
-            src="/cover-anime.png"
+            src={project.thumb.url}
             width={500}
             height={500}
-            className='rounded-3xl w-full'
+            className="rounded w-full"
             alt=""
           />
-
-          <h3 className='text-xl font-semibold py-4'>Kyuden</h3>
-          <p className={`text-xl font-light ${fraunces.className}`}>Site de anime desenvolvido para coloca em pratica meus conhecimentos de Back-end (API-kyuden), Front-end (Site Kyuden) e design.</p>
-
-          <div className='flex gap-2 py-4'>
-            <Button variant="outline" className='w-full border-2 py-8 uppercase'>Ver projeto</Button>
-            <Button variant="outline" className='w-full border-2 py-8 uppercase'>Código</Button>
-          </div>
         </div>
+
+        <h3 className="text-xl font-semibold py-4 mt-4 flex items-center gap-2">
+          <span>{project.title}</span>
+        </h3>
+        <p className={`${fraunces.className} font-light`}>
+          <span
+            dangerouslySetInnerHTML={{ __html: project.description }}
+          ></span>
+        </p>
+      </div>
+    </Link>
   )
 }
