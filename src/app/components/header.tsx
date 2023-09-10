d2705caa7bd658d5b935953b2acc659994901a46
luis.nunes <luisj2felipe09@gmail.com>
2023-09-09 21:30:52 -0300

    :lipstick: Add header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,27 +1,27 @@
-import Link from "next/link";
+import Link from 'next/link'
 
-export default function Header() {
+export function Header() {
   return (
-    <header className="z-10 px-12 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      <div className="flex flex-col">
-          <strong className="uppercase">Luis Felipe Nunes</strong>
+    <header className="z-10 px-12 max-w-5xl w-full items-center justify-between font-mono text-sm flex">
+      <Link href={'/'} className="flex flex-col">
+        <strong className="uppercase">Luis Felipe Nunes</strong>
 
-          <small>i develop</small>
-      </div>
+        <small>i develop</small>
+      </Link>
 
-        <nav className="flex gap-8">
-          <li>
-            <Link href={'/'}>inicio</Link>
-          </li>
+      <nav className="flex gap-8">
+        <li>
+          <Link href={'/'}>inicio</Link>
+        </li>
 
-          <li>
-            <Link href={'/'}>projetos</Link>
-          </li>
+        <li>
+          <Link href={'/'}>projetos</Link>
+        </li>
 
-          <li>
-            <Link href={'/'}>contato</Link>
-          </li>
-        </nav>
+        <li>
+          <Link href={'/'}>contato</Link>
+        </li>
+      </nav>
     </header>
   )
 }
