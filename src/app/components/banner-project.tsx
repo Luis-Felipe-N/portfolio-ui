0e555cc85fa2813dd4e98e0041799ee3c2656a28
luis.nunes <luisj2felipe09@gmail.com>
2023-09-09 21:33:44 -0300

    :lipstick: Cards

---
+++ src/app/components/banner-project.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+
+import { Fraunces } from 'next/font/google'
+import { ArrowRight } from './icons'
+
+const fraunces = Fraunces({ subsets: ['latin'] })
+
+interface CardProjectProps {
+  backdrop?: boolean
+}
+
+export function BannerProject({ backdrop = true }: CardProjectProps) {
+  return (
+    <div className="w-full py-12">
+      {backdrop ? (
+        <div className="bg-slate-800 rounded-3xl flex justify-center py-12">
+          <Image
+            src="/cover-anime.png"
+            width={500}
+            height={500}
+            className="rounded w-3/4"
+            alt=""
+          />
+        </div>
+      ) : (
+        <div className="flex justify-center ">
+          <Image
+            src="/cover-anime.png"
+            width={500}
+            height={500}
+            className="rounded w-full"
+            alt=""
+          />
+        </div>
+      )}
+
+      <h3 className="text-xl font-semibold py-4 flex items-center gap-2">
+        <span>Kyuden</span> <ArrowRight />
+      </h3>
+      <p className={`text-lg  ${fraunces.className} font-light`}>
+        Site com mais de 400 animes dublados e legendados
+      </p>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,82 @@
 import { Fraunces, Instrument_Sans } from 'next/font/google'
 
-import Header from './components/header'
+import { Header } from './components/header'
 import { CardProject } from './components/card-project'
+import { BannerProject } from './components/banner-project'
 
+import { api } from './lib/api'
 
 const fraunces = Fraunces({ subsets: ['latin'] })
 const instrumentSans = Instrument_Sans({ subsets: ['latin'] })
- 
 
-export default function Home() {
+export interface Project {
+  id: string
+  code: string
+  preview: string
+  createdAt: string
+  description: string
+  title: string
+  thumb: {
+    url: string
+    width?: number
+    height?: number
+  }
+  languages: string
+  video: {
+    providerUid: string
+  } | null
+}
+
+const getProjects = async (): Promise<Project[]> => {
+  const { data } = await api('/projects')
+
+  return data.projects
+}
+
+export default async function Home() {
+  const projects = await getProjects()
+
   return (
-    <main className={`flex min-h-screen flex-col items-center p-24  ${instrumentSans.className}`}>
+    <main
+      className={`flex min-h-screen flex-col items-center py-24 lg:p-24 md:px-12 sm:px-8  ${instrumentSans.className}`}
+    >
       <Header />
 
-      <section className="z-10 px-12 max-w-5xl w-full items-start justify-between flex-col text-sm lg:flex py-24">
-        <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+      <section className="z-10 px-12 max-w-5xl w-full items-start justify-between flex-col text-sm flex py-24">
+        <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight dark:text-white">
           Luis Felipe Nunes
         </h1>
 
-        <span className="pb-4 py-2 text-lg line font-mono">Desenvolvedor Fullstack</span>
+        <span className="pb-4 py-2 text-lg line font-mono">
+          Desenvolvedor Fullstack
+        </span>
 
-        <p className={`text-2xl font-light ${fraunces.className}`}>
+        <p
+          className={`text-lg sm:text-xl lg:text-2xl font-light ${fraunces.className}`}
+        >
           Estudante de Ciências da Computação apaixonado por programação, com 2
           anos de experiência em Django e focado no desenvolvimento Fullstack
           com habilidades em React, Node.js.
         </p>
       </section>
 
-      <section className="z-10 px-12 max-w-5xl w-full items-start justify-between flex-col text-sm lg:flex py-24">
-        <h2 className="text-slate-900 font-semibold text-1xl sm:text-5xl lg:text-6xl tracking-tight  dark:text-white">
+      <section className="z-10 px-12 max-w-5xl w-full items-start justify-between flex-col text-sm flex py-24">
+        <h2 className="text-slate-900 font-semibold text-xl sm:text-3xl lg:text-4xl tracking-tight  dark:text-white">
           projetos...
         </h2>
 
-        <CardProject />
-        <CardProject />
+        <p className={`text-lg mt-4 font-light ${fraunces.className}`}>
+          onde dediquei a maior parte do meu tempo, e é claro, muito estudo.
+        </p>
+
+        <BannerProject />
+        <BannerProject />
+      </section>
+
+      <section className="flex gap-4">
+        {projects.map((project) => (
+          <CardProject key={project.id} project={project} />
+        ))}
       </section>
     </main>
   )
