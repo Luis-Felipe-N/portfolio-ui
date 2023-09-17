e06f832767b7437b64ad99c5d430abcbe0b0927a
luis.nunes <luisj2felipe09@gmail.com>
2023-09-17 13:52:51 -0300

    :bug: Remove imports never use

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -1,28 +1,6 @@
-'use client'
-
 import Link from 'next/link'
 
-import { AnimatePresence, motion } from 'framer-motion'
-import { useState } from 'react'
-import {
-  AlertDialog,
-  AlertDialogTrigger,
-  AlertDialogContent,
-  AlertDialogTitle,
-  AlertDialogDescription,
-  AlertDialogCancel,
-  AlertDialogAction,
-  AlertDialogHeader,
-  AlertDialogFooter,
-} from './ui/alert-dialog'
-import { Button } from './ui/button'
-import { File, GithubLogo, LinkedinLogo } from '@phosphor-icons/react'
-
 export function Contact() {
-  const [curriculoModalIsVisible, setCurriculoModalIsVisible] = useState<
-    boolean | null
-  >(null)
-
   return (
     <section className="z-10 px-12 max-w-5xl w-full items-start justify-between flex-col text-sm flex ">
       <h2 className="text-slate-900 font-semibold text-xl sm:text-3xl lg:text-4xl tracking-tight dark:text-white">
