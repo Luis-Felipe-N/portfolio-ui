fe7d340d432d58b94860ef0ae261048b2ff625f0
luisnunes <luisj2felipe09@gmail.com>
2025-10-14 16:25:14 -0300

    chore: add fovicon

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,7 +2,6 @@ export default function pagesApp({ children }: { children: React.ReactNode }) {
 
     return (
         <div className='max-w-5xl w-full mx-auto py-12 px-12 lg:px-0'>
-
             {children}
         </div>
     )
